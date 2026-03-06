e1bd840c40a20a34ddd3ce01e6349a4ef65f3c0f
samila-albuquerque <samilaalbuquerque28git@gmail.com>
2026-03-06 19:14:50 -0300

    Código dos andares do hotel finalizado e testado

---
+++ hotel.py
@@ -0,0 +1,10 @@
+print ("--- Lista de Andares (Crescente)")
+for andar in range (1,21):
+	 if andar == 13:
+		 continue
+	 print (andar)
+print ("--- Desafio: Ordem Decrescente---")
+for andar in range(20,0,-1):
+	if andar ==13:
+		continue
+	print ("Andar:",andar)
